f2bc5a463f72c4859189b11c13c7beadbe758586
javerikass <javerikass@gmail.com>
2024-01-14 14:36:01 +0300

    feature: add exceptions

---
+++ src/main/java/ru/clevertec/house/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package ru.clevertec.house.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String errorMessage;
+    private int errorCode;
+
+}
